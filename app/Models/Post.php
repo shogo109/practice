7c47f72902b224ce681e64f7bc2edc18c4ae0b81
shogo109 <rodorigesthethird0189@gmail.com>
2023-05-10 09:07:59 +0900

    branch5
    コメント機能完了

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,18 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    Public function user()
+  Public function user()
   {
     return $this->belongsTo('App\Models\User');
   }
   //hasMany設定
-  public function likes()
+  Public function likes()
   {
     return $this->hasMany('App\Models\Like');
   }
-  public function likedBy($user)
+  Public function likedBy($user)
   {
     return Like::where('user_id', $user->id)->where('post_id', $this->id);
+  }
+  //hasMany設定
+  Public function comments()
+  {
+    return $this->hasMany('App\Models\Comment');
   }
     //　削除する必要あり？
     use HasFactory;
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -47,6 +47,19 @@
           <div>
             <span><strong>{{ $post->user->name }}</strong></span>
             <span>{{ $post->caption }}</span>
+            <div id="comment-post-{{ $post->id }}">
+              @include('post.comment_list')
+            </div>
+            <a class="light-color post-time no-text-decoration" href="/posts/{{ $post->id }}">{{ $post->created_at }}</a>
+            <hr>
+            <div class="row actions" id="comment-form-post-{{ $post->id }}">
+           	  <form class="w-100" id="new_comment" action="/posts/{{ $post->id }}/comments" accept-charset="UTF-8" data-remote="true" method="post"><input name="utf8" type="hidden" value="&#x2713;" />
+             	  {{csrf_field()}} 
+                <input value="{{ Auth::user()->id }}" type="hidden" name="user_id" />
+                <input value="{{ $post->id }}" type="hidden" name="post_id" />
+                <input class="form-control comment-input border-0" placeholder="コメント ..." autocomplete="off" type="text" name="comment" />
+              </form>
+            </div>
           </div>
         </div> 
       </div>
